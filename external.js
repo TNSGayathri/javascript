3cfcd4f83b6269c1c8d988504e0c164641149602
TNSGayathri <tnsgayathri@gmail.com>
2023-08-19 05:04:35 +0530

    main

---
+++ external.js
@@ -0,0 +1,17 @@
+var a=10;
+var b=20;
+var c=30;
+document.write(a+b,"<br>");
+document.write(a,"<br>");
+console.log(b,"<br>");
+document.write("Welcome to JavaScript</br>");
+document.write("The a value is",a,"</br>");
+alert(c);
+var name ='Gaya3';
+document.write(name+"<br>");
+name +=" Manya"
+document.write(name+"<br>")
+var name ='Gaya3';
+document.write(name.length+"<br>");
+name +=" Manya"
+document.write(name.length);
